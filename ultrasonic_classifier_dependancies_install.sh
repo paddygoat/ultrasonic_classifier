f4ed17b93381dc2196d1cbd0259721e3ae5fd934
TegwynTwmffat <paddy@goatindustries.co.uk>
2020-02-09 19:25:16 +0000

    Update ultrasonic_classifier_dependancies_install.sh

--- ultrasonic_classifier_dependancies_install.sh
+++ ultrasonic_classifier_dependancies_install.sh
@@ -41,6 +41,8 @@ make && sudo make install
 
 printf "${GREEN}Alsa installed! ${NC}\n"
 
+sudo python3.5 -m pip install git+https://github.com/abelectronicsuk/ABElectronics_Python_Libraries.git
+
 sudo apt-get -y install python3-pip
 sudo apt-get install liblzma-dev -y
 sudo apt-get install cmake -y
