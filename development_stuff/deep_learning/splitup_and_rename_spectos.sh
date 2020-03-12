3f2b7de691ddfc4a830ac6a078696504be068369
TegwynTwmffat <paddy@goatindustries.co.uk>
2020-03-12 17:34:22 +0000

    50 pixels slide rather than 100

--- development_stuff/deep_learning/splitup_and_rename_spectos.sh
+++ development_stuff/deep_learning/splitup_and_rename_spectos.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# 50 pixel sliding window.
+# All audio files need to be 384 kb/s sample rate.
+# Matplotlib settings: figure.figsize = [24,10],NFFT = 1024, noverlap = 1018, dpi = 300.
+# Specto image size will then be exactly 5828 x 1179.
+
 # cd /home/tegwyn/ultrasonic_classifier/development_stuff/deep_learning/ && bash splitup_and_rename_spectos.sh
 # Images to process should be in /home/tegwyn/ultrasonic_classifier/images/test/
 
@@ -63,8 +68,35 @@ do
 	# This produces image 8 of 8:
 	cp "$fname".jpg "$fname"_"$i"_8.jpg
 	mogrify -crop 680x680+4760+0 "$fname"_"$i"_8.jpg
-
+	
     i=2
+
+	# This produces image 1 of 8:
+	cp "$fname".jpg "$fname"_"$i"_1.jpg
+	mogrify -crop 680x680+50+0 "$fname"_"$i"_1.jpg
+	# This produces image 2 of 8:
+	cp "$fname".jpg "$fname"_"$i"_2.jpg
+	mogrify -crop 680x680+730+0 "$fname"_"$i"_2.jpg
+	# This produces image 3 of 8:
+	cp "$fname".jpg "$fname"_"$i"_3.jpg
+	mogrify -crop 680x680+1410+0 "$fname"_"$i"_3.jpg
+	# This produces image 4 of 8:
+	cp "$fname".jpg "$fname"_"$i"_4.jpg
+	mogrify -crop 680x680+2090+0 "$fname"_"$i"_4.jpg
+	# This produces image 5 of 8:
+	cp "$fname".jpg "$fname"_"$i"_5.jpg
+	mogrify -crop 680x680+2770+0 "$fname"_"$i"_5.jpg
+	# This produces image 6 of 8:
+	cp "$fname".jpg "$fname"_"$i"_6.jpg
+	mogrify -crop 680x680+3450+0 "$fname"_"$i"_6.jpg
+	# This produces image 7 of 8:
+	cp "$fname".jpg "$fname"_"$i"_7.jpg
+	mogrify -crop 680x680+4130+0 "$fname"_"$i"_7.jpg
+	# This produces image 8 of 8:
+	cp "$fname".jpg "$fname"_"$i"_8.jpg
+	mogrify -crop 680x680+4810+0 "$fname"_"$i"_8.jpg
+
+    i=3
     
 	# This produces image 1 of 8:
 	cp "$fname".jpg "$fname"_"$i"_1.jpg
@@ -87,8 +119,32 @@ do
 	# This produces image 7 of 8:
 	cp "$fname".jpg "$fname"_"$i"_7.jpg
 	mogrify -crop 680x680+4180+0 "$fname"_"$i"_7.jpg
+	
+    i=4
+    
+	# This produces image 1 of 8:
+	cp "$fname".jpg "$fname"_"$i"_1.jpg
+	mogrify -crop 680x680+150+0 "$fname"_"$i"_1.jpg
+	# This produces image 2 of 8:
+	cp "$fname".jpg "$fname"_"$i"_2.jpg
+	mogrify -crop 680x680+830+0 "$fname"_"$i"_2.jpg
+	# This produces image 3 of 8:
+	cp "$fname".jpg "$fname"_"$i"_3.jpg
+	mogrify -crop 680x680+1510+0 "$fname"_"$i"_3.jpg
+	# This produces image 4 of 8:
+	cp "$fname".jpg "$fname"_"$i"_4.jpg
+	mogrify -crop 680x680+2190+0 "$fname"_"$i"_4.jpg
+	# This produces image 5 of 8:
+	cp "$fname".jpg "$fname"_"$i"_5.jpg
+	mogrify -crop 680x680+2870+0 "$fname"_"$i"_5.jpg
+	# This produces image 6 of 8:
+	cp "$fname".jpg "$fname"_"$i"_6.jpg
+	mogrify -crop 680x680+3550+0 "$fname"_"$i"_6.jpg
+	# This produces image 7 of 8:
+	cp "$fname".jpg "$fname"_"$i"_7.jpg
+	mogrify -crop 680x680+4230+0 "$fname"_"$i"_7.jpg
 
-    i=3
+    i=5
     
 	# This produces image 1 of 8:
 	cp "$fname".jpg "$fname"_"$i"_1.jpg
@@ -112,7 +168,31 @@ do
 	cp "$fname".jpg "$fname"_"$i"_7.jpg
 	mogrify -crop 680x680+4280+0 "$fname"_"$i"_7.jpg
 	
-    i=4
+    i=6
+    
+	# This produces image 1 of 8:
+	cp "$fname".jpg "$fname"_"$i"_1.jpg
+	mogrify -crop 680x680+250+0 "$fname"_"$i"_1.jpg
+	# This produces image 2 of 8:
+	cp "$fname".jpg "$fname"_"$i"_2.jpg
+	mogrify -crop 680x680+930+0 "$fname"_"$i"_2.jpg
+	# This produces image 3 of 8:
+	cp "$fname".jpg "$fname"_"$i"_3.jpg
+	mogrify -crop 680x680+1610+0 "$fname"_"$i"_3.jpg
+	# This produces image 4 of 8:
+	cp "$fname".jpg "$fname"_"$i"_4.jpg
+	mogrify -crop 680x680+2290+0 "$fname"_"$i"_4.jpg
+	# This produces image 5 of 8:
+	cp "$fname".jpg "$fname"_"$i"_5.jpg
+	mogrify -crop 680x680+2970+0 "$fname"_"$i"_5.jpg
+	# This produces image 6 of 8:
+	cp "$fname".jpg "$fname"_"$i"_6.jpg
+	mogrify -crop 680x680+3650+0 "$fname"_"$i"_6.jpg
+	# This produces image 7 of 8:
+	cp "$fname".jpg "$fname"_"$i"_7.jpg
+	mogrify -crop 680x680+4330+0 "$fname"_"$i"_7.jpg
+	
+    i=7
     
 	# This produces image 1 of 8:
 	cp "$fname".jpg "$fname"_"$i"_1.jpg
@@ -135,8 +215,32 @@ do
 	# This produces image 7 of 8:
 	cp "$fname".jpg "$fname"_"$i"_7.jpg
 	mogrify -crop 680x680+4380+0 "$fname"_"$i"_7.jpg
+	
+    i=8
     
-    i=5
+	# This produces image 1 of 8:
+	cp "$fname".jpg "$fname"_"$i"_1.jpg
+	mogrify -crop 680x680+350+0 "$fname"_"$i"_1.jpg
+	# This produces image 2 of 8:
+	cp "$fname".jpg "$fname"_"$i"_2.jpg
+	mogrify -crop 680x680+1030+0 "$fname"_"$i"_2.jpg
+	# This produces image 3 of 8:
+	cp "$fname".jpg "$fname"_"$i"_3.jpg
+	mogrify -crop 680x680+1710+0 "$fname"_"$i"_3.jpg
+	# This produces image 4 of 8:
+	cp "$fname".jpg "$fname"_"$i"_4.jpg
+	mogrify -crop 680x680+2390+0 "$fname"_"$i"_4.jpg
+	# This produces image 5 of 8:
+	cp "$fname".jpg "$fname"_"$i"_5.jpg
+	mogrify -crop 680x680+3070+0 "$fname"_"$i"_5.jpg
+	# This produces image 6 of 8:
+	cp "$fname".jpg "$fname"_"$i"_6.jpg
+	mogrify -crop 680x680+3750+0 "$fname"_"$i"_6.jpg
+	# This produces image 7 of 8:
+	cp "$fname".jpg "$fname"_"$i"_7.jpg
+	mogrify -crop 680x680+4430+0 "$fname"_"$i"_7.jpg
+    
+    i=9
     
 	# This produces image 1 of 8:
 	cp "$fname".jpg "$fname"_"$i"_1.jpg
@@ -160,7 +264,31 @@ do
 	cp "$fname".jpg "$fname"_"$i"_7.jpg
 	mogrify -crop 680x680+4480+0 "$fname"_"$i"_7.jpg
 	
-    i=6
+    i=10
+    
+	# This produces image 1 of 8:
+	cp "$fname".jpg "$fname"_"$i"_1.jpg
+	mogrify -crop 680x680+450+0 "$fname"_"$i"_1.jpg
+	# This produces image 2 of 8:
+	cp "$fname".jpg "$fname"_"$i"_2.jpg
+	mogrify -crop 680x680+1130+0 "$fname"_"$i"_2.jpg
+	# This produces image 3 of 8:
+	cp "$fname".jpg "$fname"_"$i"_3.jpg
+	mogrify -crop 680x680+1810+0 "$fname"_"$i"_3.jpg
+	# This produces image 4 of 8:
+	cp "$fname".jpg "$fname"_"$i"_4.jpg
+	mogrify -crop 680x680+2490+0 "$fname"_"$i"_4.jpg
+	# This produces image 5 of 8:
+	cp "$fname".jpg "$fname"_"$i"_5.jpg
+	mogrify -crop 680x680+3170+0 "$fname"_"$i"_5.jpg
+	# This produces image 6 of 8:
+	cp "$fname".jpg "$fname"_"$i"_6.jpg
+	mogrify -crop 680x680+3850+0 "$fname"_"$i"_6.jpg
+	# This produces image 7 of 8:
+	cp "$fname".jpg "$fname"_"$i"_7.jpg
+	mogrify -crop 680x680+4530+0 "$fname"_"$i"_7.jpg
+	
+    i=11
     
 	# This produces image 1 of 8:
 	cp "$fname".jpg "$fname"_"$i"_1.jpg
@@ -184,7 +312,31 @@ do
 	cp "$fname".jpg "$fname"_"$i"_7.jpg
 	mogrify -crop 680x680+4580+0 "$fname"_"$i"_7.jpg
 	
-    i=7
+    i=12
+    
+	# This produces image 1 of 8:
+	cp "$fname".jpg "$fname"_"$i"_1.jpg
+	mogrify -crop 680x680+550+0 "$fname"_"$i"_1.jpg
+	# This produces image 2 of 8:
+	cp "$fname".jpg "$fname"_"$i"_2.jpg
+	mogrify -crop 680x680+1230+0 "$fname"_"$i"_2.jpg
+	# This produces image 3 of 8:
+	cp "$fname".jpg "$fname"_"$i"_3.jpg
+	mogrify -crop 680x680+1910+0 "$fname"_"$i"_3.jpg
+	# This produces image 4 of 8:
+	cp "$fname".jpg "$fname"_"$i"_4.jpg
+	mogrify -crop 680x680+2590+0 "$fname"_"$i"_4.jpg
+	# This produces image 5 of 8:
+	cp "$fname".jpg "$fname"_"$i"_5.jpg
+	mogrify -crop 680x680+3270+0 "$fname"_"$i"_5.jpg
+	# This produces image 6 of 8:
+	cp "$fname".jpg "$fname"_"$i"_6.jpg
+	mogrify -crop 680x680+3950+0 "$fname"_"$i"_6.jpg
+	# This produces image 7 of 8:
+	cp "$fname".jpg "$fname"_"$i"_7.jpg
+	mogrify -crop 680x680+4630+0 "$fname"_"$i"_7.jpg
+	
+    i=13
     
 	# This produces image 1 of 8:
 	cp "$fname".jpg "$fname"_"$i"_1.jpg
@@ -207,6 +359,30 @@ do
 	# This produces image 7 of 8:
 	cp "$fname".jpg "$fname"_"$i"_7.jpg
 	mogrify -crop 680x680+4680+0 "$fname"_"$i"_7.jpg
+	
+    i=14
+    
+	# This produces image 1 of 8:
+	cp "$fname".jpg "$fname"_"$i"_1.jpg
+	mogrify -crop 680x680+650+0 "$fname"_"$i"_1.jpg
+	# This produces image 2 of 8:
+	cp "$fname".jpg "$fname"_"$i"_2.jpg
+	mogrify -crop 680x680+1330+0 "$fname"_"$i"_2.jpg
+	# This produces image 3 of 8:
+	cp "$fname".jpg "$fname"_"$i"_3.jpg
+	mogrify -crop 680x680+2010+0 "$fname"_"$i"_3.jpg
+	# This produces image 4 of 8:
+	cp "$fname".jpg "$fname"_"$i"_4.jpg
+	mogrify -crop 680x680+2690+0 "$fname"_"$i"_4.jpg
+	# This produces image 5 of 8:
+	cp "$fname".jpg "$fname"_"$i"_5.jpg
+	mogrify -crop 680x680+3370+0 "$fname"_"$i"_5.jpg
+	# This produces image 6 of 8:
+	cp "$fname".jpg "$fname"_"$i"_6.jpg
+	mogrify -crop 680x680+4050+0 "$fname"_"$i"_6.jpg
+	# This produces image 7 of 8:
+	cp "$fname".jpg "$fname"_"$i"_7.jpg
+	mogrify -crop 680x680+4730+0 "$fname"_"$i"_7.jpg
 	
 done
 
